13dc31cc3173ed752639467fa5a671e9e9428c83
Abdulazeez Saidu <saeeduabdulazeez@gmail.com>
2025-09-08 00:26:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+          document.querySelector(this.getAttribute('href'))
+                .scrollIntoView({ behavior: 'smooth' });
+                  });
+                  });
+
+
